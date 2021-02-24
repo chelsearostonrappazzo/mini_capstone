6d47222df129c4e25ef69072f985de8063fe3ec1
Chelsea Roston <cmroston@gmail.com>
2021-02-24 15:03:54 -0500

    adds products via seed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Product.create(name: "Venetian Renaissance Gown", price: 450, description: "Imagine yourself walking along the Great canal in 1480 Venice. It's just what you need when you attend Carnival. It can be crafted in your choice of fabric!")
+# Product.create(name: "Regency Evening Gown", price: 300, description: "Run into Mr. Darcy's arms in this finely-crafted gown of silver net. You may just be handsome enough to tempt him.")
+# Product.create(name: "First Bustle Era Gown", price: 350, description: "Enjoy this frothy confection of striped rose pink silk taffeta. You can pretend you are walking along the hallways of Bath.")
