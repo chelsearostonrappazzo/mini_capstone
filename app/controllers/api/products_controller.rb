2c090c775f579520e7c0763366ce2240f9e1b4ae
Chelsea Roston <cmroston@gmail.com>
2021-02-24 15:16:46 -0500

    adds JSON for each individual product

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -3,4 +3,19 @@ class Api::ProductsController < ApplicationController
     @all_products = Product.all
     render "all_products.json.jb"
   end
+
+  def one
+    @product_one = Product.first
+    render "product_one.json.jb"
+  end
+
+  def two
+    @product_two = Product.find_by(id: 2)
+    render "product_two.json.jb"
+  end
+
+  def three
+    @product_three = Product.first
+    render "product_three.json.jb"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
     get "/all_products" => "products#all"
+    get "/product_one" => "products#one"
+    get "/product_two" => "products#two"
+    get "/product_three" => "products#three"
   end
 end
