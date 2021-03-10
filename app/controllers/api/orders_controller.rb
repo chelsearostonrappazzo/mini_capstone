3ab621a72a31e95980d8f4ce296324173f64b4c4
Chelsea Roston <cmroston@gmail.com>
2021-03-10 13:46:46 -0500

    updates show and index for order model

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -1,16 +1,35 @@
 class Api::OrdersController < ApplicationController
   def create
+    product = Product.find_by(id: params[:product_id])
+
+    calculated_subtotal = product.price * params[:quantity].to_i
+    calculated_tax = calculated_subtotal * 0.09
+    calculated_total = calculated_subtotal + calculated_tax
+
     if current_user
       @order = Order.new(
         user_id: current_user,
         product_id: params[:product_id],
         quantity: params[:quantity],
-        subtotal: 
+        subtotal: calculated_subtotal,
+        tax: calculated_tax,
+        total: calculated_total,
       )
       @order.save
-      render json: { message: "Order submitted!" }
+      render "show.json.jb"
     else
       render json: { error: "You must be logged in to submit an order!" }
     end
+  end
+
+  def index
+    @orders = current_user.orders
+    render "index.json.jb"
+  end
+
+  def show
+    order_id = params[:id]
+    @order = current_user.orders.find(order_id)
+    render "show.json.jb"
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,15 +2,15 @@ class Order < ApplicationRecord
   belongs_to :product
   belongs_to :user, optional: true
 
-  def subtotal
-    product.price * quantity
-  end
+  # def subtotal
+  #   product.price * quantity
+  # end
 
-  def tax
-    subtotal * 0.09
-  end
+  # def tax
+  #   subtotal * 0.09
+  # end
 
-  def total
-    subtotal + total
-  end
+  # def total
+  #   subtotal + total
+  # end
 end
